381dfc0b6d8b66b080100e015eba628a3c3fe306
Patrick Huynh <patrick@patrickh.ca>
2022-10-23 14:39:07 -0700

    Submit Friends Book Assignment

---
+++ Friends Book/src/Friend.java
@@ -0,0 +1,37 @@
+public class Friend {
+
+    // Friend Attributes
+    private final String firstName;
+    private final String lastName;
+    private final String phoneNumber;
+    private final String email;
+
+    Friend(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return firstName.toUpperCase() + " " + lastName.toUpperCase();
+    }
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
